c862b69c87ab74b34126707d6ad1a39bf7c0910e
Yannick <Yannick@1a3eb412-a335-0410-b833-90f453dfd2c8>
2003-12-29 15:47:17 +0000

    no message

---
+++ MTDSP/MTCatmullASM.h
@@ -0,0 +1,9 @@
+//---------------------------------------------------------------------------
+#ifndef MTCATMULLASM_INCLUDED
+#define MTCATMULLASM_INCLUDED
+//---------------------------------------------------------------------------
+#include "../Headers/MTXExtension.h"
+//---------------------------------------------------------------------------
+void MTACT a_catmullinterpolate(double x1,sample p1,double x2,sample p2,double x3,sample p3,double x4,sample p4,double xf,double xt,int resolution);
+//---------------------------------------------------------------------------
+#endif
