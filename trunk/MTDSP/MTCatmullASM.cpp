f3e8b3016b016edd616446eee536a775df81ddc2
Yannick <Yannick@1a3eb412-a335-0410-b833-90f453dfd2c8>
2003-12-29 15:47:17 +0000

    no message

---
+++ trunk/MTDSP/MTCatmullASM.cpp
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+//
+//	MadTracker DSP Core
+//
+//		Platforms:	All
+//		Processors:	x86
+//
+//	Copyright © 1999-2003 Yannick Delwiche. All rights reserved.
+//
+//---------------------------------------------------------------------------
+#include "MTCatmullASM.h"
+//---------------------------------------------------------------------------
+void MTACT a_catmullinterpolate(double x1,sample p1,double x2,sample p2,double x3,sample p3,double x4,sample p4,double xf,double xt,int resolution)
+{
+	_asm{
+
+	};
+}
+//---------------------------------------------------------------------------
