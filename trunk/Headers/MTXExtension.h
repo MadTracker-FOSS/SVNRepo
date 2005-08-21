972d879f7b6f0f1a549ebfa0f680ce88cbbfd7b9
Yannick <Yannick@1a3eb412-a335-0410-b833-90f453dfd2c8>
2005-08-21 10:26:23 +0000

    no message

--- trunk/Headers/MTXExtension.h
+++ trunk/Headers/MTXExtension.h
@@ -288,7 +288,7 @@ public:
 	virtual int MTCT processinput(const char *input){ return 0; };
 };
 
-typedef MTXInterfaces* MTACT MTXMainCall(MTInterface *mti);
+typedef MTXInterfaces* MTXMainCall(MTInterface *mti);
 //---------------------------------------------------------------------------
 //	Swapping functions to convert little<->big endian
 //---------------------------------------------------------------------------
