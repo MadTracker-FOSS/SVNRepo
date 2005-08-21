65886de1eedebfa14483e2b77630240ad7e3993b
Yannick <Yannick@1a3eb412-a335-0410-b833-90f453dfd2c8>
2005-08-21 10:00:44 +0000

    no message

--- MTSystem/MTMD5.h
+++ MTSystem/MTMD5.h
@@ -1,4 +1,15 @@
 //---------------------------------------------------------------------------
+//
+//	MadTracker System Core
+//
+//		Platforms:	All
+//		Processors: All
+//
+//	Copyright © 1999-2006 Yannick Delwiche. All rights reserved.
+//
+//	$Id$
+//
+//---------------------------------------------------------------------------
 // Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
 // rights reserved.
 // 
