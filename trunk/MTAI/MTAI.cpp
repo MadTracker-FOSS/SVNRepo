f4a86edc500ae5fa3dcf3355f4cce254e9e6559f
Yannick <Yannick@1a3eb412-a335-0410-b833-90f453dfd2c8>
2005-09-06 16:22:34 +0000

    no message

---
+++ trunk/MTAI/MTAI.cpp
@@ -0,0 +1,28 @@
+//---------------------------------------------------------------------------
+//
+//	MadTracker Artificial Intelligence
+//
+//		Platforms:	All
+//		Processors: All
+//
+//	Copyright © 1999-2006 Yannick Delwiche. All rights reserved.
+//
+//	$Id$
+//
+//---------------------------------------------------------------------------
+#ifdef _WIN32
+#	include <windows.h>
+#endif
+//---------------------------------------------------------------------------
+#ifdef _WIN32
+BOOL APIENTRY DllMain(HANDLE hModule,DWORD ul_reason_for_call,LPVOID lpReserved)
+{
+	return TRUE;
+}
+#else
+int main(int argc,const char* argv[])
+{
+	return 0;
+}
+#endif
+//---------------------------------------------------------------------------
